0f191e3aa2a11429bfa8db589cae78bef782aebb
Purplemet CI <ci@purplemet.com>
2026-04-28 05:27:07 +0200

    Update Homebrew formula to v1.1.14

--- Formula/purplemet-cli.rb
+++ Formula/purplemet-cli.rb
@@ -1,34 +1,43 @@
 class PurplemetCli < Formula
   desc "CLI for Purplemet web application security analysis"
   homepage "https://purplemet.com"
-  version "1.1.12"
+  version "1.1.14"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.12/purplemet-cli-darwin-arm64"
-      sha256 "92cc0d8173f677f768903ffcc1129e3d49d9fb900feae6034925a014f87b0408"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.14/purplemet-cli-darwin-arm64"
+      sha256 "ed1c5c80e6625b0e52cd9969712724ddf91c4d97e7b43e29cf970bf601650c25"
     else
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.12/purplemet-cli-darwin-amd64"
-      sha256 "fd4660248e6116fdd929c3eef7d9f04e1a09650ba35dd6f3432499f5fb4356c7"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.14/purplemet-cli-darwin-amd64"
+      sha256 "3a1ba2cdb6e6f9dd65f05f760ee6908281ec1a1fd6f09d431518c24407f9f959"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.12/purplemet-cli-linux-arm64"
-      sha256 "210322c5579598ce78eeff4fda4601a5eda229bd4e934520b37ecda2be67e9ca"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.14/purplemet-cli-linux-arm64"
+      sha256 "15de1912e498439a5821f4765814111901fb332fb0c054c8d441ebc715ac3c59"
     else
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.12/purplemet-cli-linux-amd64"
-      sha256 "69eed1e598f3c65dab488db65729f39d615d946797f21666e460d650dc052171"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.14/purplemet-cli-linux-amd64"
+      sha256 "4c8121b3dd71117629f3b579ab553a1854fc581678503400880270cd40a98e86"
     end
+  end
+
+  resource "completions" do
+    url "https://github.com/Purplemet/cli/releases/download/v1.1.14/completions.tar"
+    sha256 "97e220c6b30f36c937444929d91227a09228e30841beb8c09008921b6b8618c3"
   end
 
   def install
     binary = Dir["purplemet-cli-*"].first || "purplemet-cli"
     bin.install binary => "purplemet-cli"
 
-    generate_completions_from_executable(bin/"purplemet-cli", "completion")
+    resource("completions").stage do
+      bash_completion.install "purplemet-cli.bash"
+      zsh_completion.install  "_purplemet-cli"
+      fish_completion.install "purplemet-cli.fish"
+    end
   end
 
   test do
