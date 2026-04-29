539f0a33a2b7b54ddabd7089ab56eee85e20f461
Purplemet CI <ci@purplemet.com>
2026-04-30 01:40:36 +0200

    Update Homebrew formula to v1.1.21

--- Formula/purplemet-cli.rb
+++ Formula/purplemet-cli.rb
@@ -1,36 +1,36 @@
 class PurplemetCli < Formula
   desc "CLI for Purplemet web application security analysis"
   homepage "https://purplemet.com"
-  version "1.1.20"
+  version "1.1.21"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.20/purplemet-cli-darwin-arm64"
-      sha256 "1339dbf1cb6b223c382dc0d08cbc0e28421a7a8d8fce2bf1046ebbb11fd5915c"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.21/purplemet-cli-darwin-arm64"
+      sha256 "2c122f9f51b2664576efc5929df2f1369e143c83ce34c0aee529b74099dd85ef"
     else
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.20/purplemet-cli-darwin-amd64"
-      sha256 "1277e60a834b471c1736cde37948363e65102aea7ab10c95f72aff6704002008"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.21/purplemet-cli-darwin-amd64"
+      sha256 "14ba3195404229d8ab017c2e22bf49e6428b017b5f040f897c04270d7ac3bb0e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.20/purplemet-cli-linux-arm64"
-      sha256 "79e9aedc811fd64444bdc2c1b3b7f668c893bbcaf7b20168e0de2ea153464a05"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.21/purplemet-cli-linux-arm64"
+      sha256 "a6aafa4f0df7242ed3606e91edebbcddddb90dba253fae33cede1608e6c5ae75"
     else
-      url "https://github.com/Purplemet/cli/releases/download/v1.1.20/purplemet-cli-linux-amd64"
-      sha256 "7f162c6e17947e0109a5df83e810c10e6586dc28ba155cbbf378ad89a64d08f1"
+      url "https://github.com/Purplemet/cli/releases/download/v1.1.21/purplemet-cli-linux-amd64"
+      sha256 "d15ae7037f01dea6d2c48c7c3c7679e5646220f0763c50eea4e34663b35be9bd"
     end
   end
 
   resource "completions" do
-    url "https://github.com/Purplemet/cli/releases/download/v1.1.20/completions.tar"
+    url "https://github.com/Purplemet/cli/releases/download/v1.1.21/completions.tar"
     sha256 "97e220c6b30f36c937444929d91227a09228e30841beb8c09008921b6b8618c3"
   end
 
   resource "man" do
-    url "https://github.com/Purplemet/cli/releases/download/v1.1.20/man.tar"
+    url "https://github.com/Purplemet/cli/releases/download/v1.1.21/man.tar"
     sha256 "c81b5737d74ef8a03e6bc6098c8f93a4f1ab6839d46c0635eb1480035e309b96"
   end
 
